2f8e6adbbc9516089c30f1cb85c546323ec94377
Marta <mavidelaru@gmail.com>
2020-01-28 18:38:49 +0100

    I've added some modals

--- hotel_animacion.js
+++ hotel_animacion.js
@@ -1,58 +1,65 @@
+let hoteles = [];
 
-
-var hoteles = [];
-
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
 
 function crearHotel() {
 
-    var nombre = prompt("Nombre del hotel");
-    var habitaciones = prompt("Número de habitaciones");
-    var plantas = prompt("Número de plantas");
-    var superficie = prompt("¿Cuál es la superficie del hotel?")
-    var hotel = new Hotel(nombre, habitaciones, plantas, superficie);
-
+    let nombre = document.getElementById("nameh").value;
+    let habitaciones = document.getElementById("nhab").value;
+    let plantas = document.getElementById("nplan").value; 
+    let superficie = document.getElementById("shot").value;
+    let hotel = new Hotel(nombre, habitaciones, plantas, superficie);
     let aumento = hoteles.push(hotel);
 
-    console.log(hoteles);
+    alert("Tu hotel se ha AÑADIDO satisfactoriamente");
 
-    alert("Tu hotel se ha CREADO satisfactoriamente");
-    document.getElementById("answer").innerHTML = "Hay " + hoteles.length + " hoteles inscritos";
+    if (hoteles.length==0){
+        document.getElementById("answer").innerHTML = "No tienes ningún hotel guardado";
+    }else if(hoteles.length<2){
+        document.getElementById("answer").innerHTML = "Hay " + hoteles.length + " hotel guardado";
+    }
+    else {
+        document.getElementById("answer").innerHTML = "Hay " + hoteles.length + " hoteles guardados";
+    }
 
+    $('#myModal').modal('toggle');
 }
 
 function borrarHotel() {
 
-    var name = prompt("¿Qué hotel quieres borrar?");
+    let name = prompt("¿Qué hotel quieres borrar?");
 
     for (i = hoteles.length - 1; i >= 0; i--) {
-
-        var hotelPosition = hoteles[i];
+        let hotelPosition = hoteles[i];
         if (hotelPosition._nombre === name) {
             hoteles.splice(i, 1);
         }
     }
 
     alert("Tu hotel se ha BORRADO satisfactoriamente");
-    console.log(hoteles);
-    document.getElementById("answer").innerHTML = "Hay "+hoteles.length+" hoteles inscritos";
 
+    //Div donde se muestran los hoteles que hay
+    if (hoteles.length<2){
+        document.getElementById("answer").innerHTML = "Hay " + hoteles.length + " hotel guardado";
+    }else {
+        document.getElementById("answer").innerHTML = "Hay " + hoteles.length + " hoteles guardados";
+    }
 }
 
 function verHotel() {
 
-    var name = prompt("¿Qué hotel quieres borrar?");
-
+    let name = prompt("¿Qué hotel quieres borrar?");
     let servicio = 0;
-
     let flag = false;
 
     for (i = hoteles.length - 1; i >= 0; i--) {
-
-        var hotelPosition = hoteles[i];
+        let hotelPosition = hoteles[i];
         if (hotelPosition._nombre === name) {
 
             servicio = hotelPosition.calcularManteniment(servicio);
-            var show = "El hotel que buscas se llama " + hotelPosition.nombre + "</br>";
+            let show = "El hotel que buscas se llama " + hotelPosition.nombre + "</br>";
             show += "El número de habitaciones es " + hotelPosition.habitaciones + "</br>";
             show += "El número de plantas es " + hotelPosition.plantas + "</br>";
             show += "Con una superficie de " + hotelPosition.superficie + " metros cuadrados</br>";
@@ -67,36 +74,31 @@ function verHotel() {
 
         document.getElementById("showHotel").innerHTML = "Lo siento, no hemos encontrado tu hotel";
     }
-
-
 }
-
 
 function modificarHotel() {
 
-    var name = prompt("¿Qué hotel quieres modificar?");
-    var counter = hoteles.length - 1;
+    let name = prompt("¿Qué hotel quieres modificar?");
+    let counter = hoteles.length - 1;
 
     for (i = hoteles.length - 1; i >= 0; i--) {
 
-        
         if (hoteles[i]._nombre == name) {
-
-            var cambio = parseInt(prompt("Qué quieres cambiar? 1- Nombre 2- Habitaciones 3- Plantas 4- Superficie"));
-            var newData = prompt("Introduce el nuevo dato")
+            let cambio = parseInt(prompt("Qué quieres cambiar? 1- Nombre 2- Habitaciones 3- Plantas 4- Superficie"));
+            let newData = prompt("Introduce el nuevo dato")
            
             switch (cambio) {
                 case 1:
-                        hoteles[i].nombre=newData;
+                    hoteles[i].nombre=newData;
                     break;
                 case 2:
-                        hoteles[i].habitaciones=newData;
+                    hoteles[i].habitaciones=newData;
                     break;
                 case 3 :
-                        hoteles[i].plantas=newData;
+                    hoteles[i].plantas=newData;
                     break;
                 case 4 :
-                        hoteles[i].superficie=newData;
+                    hoteles[i].superficie=newData;
                     break;
                 default:
                     alert("Algo ha salido mal. Pruebalo de nuevo");        
@@ -104,9 +106,7 @@ function modificarHotel() {
             console.log(hoteles);
             document.getElementById("showHotel").innerHTML = "Se han realizado los cambios en el hotel "+ name;
 
-        
         } else {
-
             counter--
         }
 
